b683a47738dc0d69a7982227e95795586d8dcd00
Prof Vinicius de Oliveira Almeida <92415933+valmeidavr@users.noreply.github.com>
2022-08-20 13:14:53 -0300

    Link Logo

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -2,6 +2,7 @@
     @php
         $activePage = basename($_SERVER['PHP_SELF']) ;
     @endphp
+    <li class="<?= ($activePage == 'quemsomos') ? 'isActive':''; ?>"><a href="/">Quem Somos</a></li>
     <li class="<?= ($activePage == 'cursos') ? 'isActive':''; ?>"><a href="/">Cursos</a></li>
-    <li class="<?= ($activePage == 'contato') ? 'isActive':''; ?>"><a href="/contato">Contato</a></li>
+    <li class="<?= ($activePage == 'faleconosco') ? 'isActive':''; ?>"><a href="/">Fale Conosco</a></li>
 @endsection
