186fb216501ca454c4b81b06edd15441505c89de
reinaldo <reinalux@gmail.com>
2022-10-08 10:58:42 -0300

    Atualizacao

---
+++ database/migrations/2022_10_08_135502_create_comentarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('aluno_id');
+            $table->longText('descricao');
+            $table->unsignedBigInteger('aulas_id');
+                $table->foreign('aulas_id')->references('id')->on('aulas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
